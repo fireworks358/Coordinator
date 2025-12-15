7ae2fcfd9ac461ce5485a500294ea17cb6e564f8
fireworks358 <fireworks358@gmail.com>
2025-12-15 00:34:26 +0000

    2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // This line ensures the app runs on localhost:3000 by default
-    port: 3000, 
+  base: "/fireworks358",
   }
 });
